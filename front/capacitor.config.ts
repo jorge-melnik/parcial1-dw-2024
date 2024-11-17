ca4d3b34ee68d6929c4aeb2760e8b4af8a94e107
Jorge Melnik <jmelnik19@gmail.com>
2024-11-17 00:13:36 -0300

    capacitor 6

--- front/capacitor.config.ts
+++ front/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'front',
   appName: 'front',
-  webDir: 'www'
+  webDir: 'dist/front/browser',
 };
 
 export default config;
